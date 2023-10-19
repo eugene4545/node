6cbe346f99835a776fb4b96f966d28644c248f29
Eugene <129702251+eugene4545@users.noreply.github.com>
2023-10-19 20:21:03 +0100

    password-generator.js

--- password-generator.js
+++ password-generator.js
@@ -2,7 +2,7 @@ const generator = require("generate-password");
 
 // Generate a random password
 const password = generator.generate({
-  length: 12,
+  length: 12, //maximun of 12 characters
   numbers: true,
   symbols: true,
   uppercase: true,
