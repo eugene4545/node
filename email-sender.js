a094268e4727d6a2e8c9700518a6aaa5576a27b4
Eugene <129702251+eugene4545@users.noreply.github.com>
2023-10-19 20:14:28 +0100

    Update email-sender.js

--- email-sender.js
+++ email-sender.js
@@ -1,8 +1,8 @@
 const nodemailer = require("nodemailer");
 
-// Create a transporter with your email service details
+// Creating a transporter with  email service details
 const transporter = nodemailer.createTransport({
-  service: "Gmail", // e.g., "Gmail" for Gmail
+  service: "Gmail", 
   auth: {
     user: "odera7070@gmail.com",
     pass: "xsna oprz xcqx kvho",
@@ -17,7 +17,7 @@ const mailOptions = {
   from: "odera7070@gmail.com",
   to: "odera7070@gmail.com",
   subject: "Node.js Email Test",
-  text: "This is a test email sent from Node.js.",
+  text: "Hello there! from Node.js.",
 };
 
 // Send the email
