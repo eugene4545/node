2a551ad439ba76a14987842555b892db322d87db
Eugene <129702251+eugene4545@users.noreply.github.com>
2023-10-19 20:13:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
   res.end("<h1>Hello Node!!!!</h1>\n");
 });
 
-const PORT = 3000;
+const PORT = 3000; //our designated port
 server.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}/`);
 });
